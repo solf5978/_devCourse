a843b8724334fbe8e63ea547661026b5ef581e70
_temp <78365254+solf5978@users.noreply.github.com>
2023-09-30 20:53:12 +0800

    introduced fmt.sprintf

--- _basicOfGo_FM/src/07importReadTXTFile.go
+++ _basicOfGo_FM/src/07importReadTXTFile.go
@@ -1,13 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	parentPath, _ := os.Getwd()
 	filename := "Path To The File"
 
-	content, err := src.ReadTextFile(filename)
+	content, err := ReadTextFile(parentPath + "/" + filename)
 	if err != nil {
 		fmt.Println(content)
+		newContent := "Original: " + content + "\n Double the content"
+		newContent = fmt.Sprintf("Original: %v \n double the content", content)
+		err := WriteToFile(filename+".output", newContent)
+		if err != nil {
+			return
+		}
 	} else {
 		fmt.Printf("Panic at %v", err)
 	}
