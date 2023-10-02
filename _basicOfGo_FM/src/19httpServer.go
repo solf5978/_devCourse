b2a493bb0a986ee5a0ab742e2136503c4cafedc3
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-02 21:33:31 +0800

    adding function handle with template

--- _basicOfGo_FM/src/19httpServer.go
+++ _basicOfGo_FM/src/19httpServer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -9,11 +10,22 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello From A Go Program"))
 }
 
+func handleTemplate(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("templates.index.tmpl")
+	if err != nil {
+		w.Write([]byte("Internal Server Error"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	} else {
+		html.Execute(w, "Test")
+	}
+}
+
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc(
 		"/", handleHello)
-
+	server.HandleFunc("/template", handleTemplate)
 	fs := http.FileServer(http.Dir("./public"))
 	server.Handle("/", fs)
 	err := http.ListenAndServe(":3333", nil)
