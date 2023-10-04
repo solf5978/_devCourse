899cc3109520cafaf07537e47a1c2325357d1df1
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-04 23:54:56 +0800

    switch cases

--- _fullTimeGoDev/03enums.go
+++ _fullTimeGoDev/03enums.go
@@ -1,1 +1,24 @@
 package _fullTimeGoDev
+
+import "fmt"
+
+type WeaponType int
+
+func getDamage(weaponType string) int {
+	switch weaponType {
+	case "axe":
+		return 100
+	case "sword":
+		return 90
+	case "woodenStick":
+		return 1
+	case "knife":
+		return 40
+	default:
+		panic("Don't get it without a weapon")
+	}
+}
+
+func main() {
+	fmt.Println("damage of weapon:", getDamage("woodenStick"))
+}
