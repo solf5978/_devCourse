37c1ac304c6547a6efe3e114a1975fc2c3b381de
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-05 19:23:04 +0800

    Using For / Fori

---
+++ _fullTimeGoDev/04flowControl.go
@@ -0,0 +1,16 @@
+package _fullTimeGoDev
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("Current:", i)
+	}
+
+	number := []int{1, 2, 3, 4, 5, 6, 7}
+
+	for i := 0; i < len(number); i++ {
+		fmt.Println(i)
+		fmt.Println(number[i])
+	}
+}
