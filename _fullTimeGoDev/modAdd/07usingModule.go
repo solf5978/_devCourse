03b6be2f540d47de7da4e8dbfc97a3ce1b6e4bf7
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-05 22:20:31 +0800

    function working fine, but cannot file the module when execute

--- _fullTimeGoDev/modAdd/07usingModule.go
+++ _fullTimeGoDev/modAdd/07usingModule.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	randNum := getNumber()
+	randNum := getRand()
 	fmt.Println(randNum)
+
+	user := User{
+		username: "Jason",
+		age:      getRand(),
+	}
+	fmt.Printf("USER: %s | Age: %d\n", user.username, user.age)
+}
+
+func getRand() int {
+	return rand.Int()
 }
