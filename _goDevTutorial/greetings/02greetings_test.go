e45e2b35ee1b9210f7cb824c7878fa7c9a9d73ca
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-07 11:16:17 +0800

    Build and Export without needing to have path specified

--- _goDevTutorial/greetings/02greetings_test.go
+++ _goDevTutorial/greetings/02greetings_test.go
@@ -5,18 +5,11 @@ import (
 	"testing"
 )
 
-package greetings
-
-import (
-"testing"
-"regexp"
-)
-
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
 func TestHelloName(t *testing.T) {
 	name := "Gladys"
-	want := regexp.MustCompile(`\b`+name+`\b`)
+	want := regexp.MustCompile(`\b` + name + `\b`)
 	msg, err := Hello("Gladys")
 	if !want.MatchString(msg) || err != nil {
 		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
