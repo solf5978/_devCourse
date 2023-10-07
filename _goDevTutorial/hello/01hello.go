148ba73d8174f5d1be52f7e69585ab642f5b2467
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-07 09:46:04 +0800

    refine the return varaiables

--- _goDevTutorial/hello/01hello.go
+++ _goDevTutorial/hello/01hello.go
@@ -3,12 +3,19 @@ package main
 import (
 	"example/greetings"
 	"fmt"
+	"log"
 	"rsc.io/quote"
 )
 
 func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("Gladys")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
