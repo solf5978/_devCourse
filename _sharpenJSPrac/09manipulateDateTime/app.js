1c075765caeb856904f5d35ce2085cbf8d25a9a2
_temp <78365254+solf5978@users.noreply.github.com>
2023-12-24 11:42:09 +0800

    manipulate datetime starter

---
+++ _sharpenJSPrac/09manipulateDateTime/app.js
@@ -0,0 +1,14 @@
+
+const determineAgeFunction = function(day, month, year) {
+  
+  console.log(`You are ${years} years and ${days} days old. Which is ${hours} hours OR ${minutes} minutes OR ${seconds} seconds.`);
+};
+
+
+
+// Testing determineAgeFunction...
+
+determineAgeFunction(25,5,1965);
+determineAgeFunction(1,1,1970);
+determineAgeFunction(03,09,2001);
+determineAgeFunction(27,04,2023);
