71328cc4520a96bee34ced00b32806c874f1bc1e
_temp <78365254+solf5978@users.noreply.github.com>
2023-12-24 14:37:21 +0800

    recursion on multiply number

---
+++ _sharpenJSPrac/14prodOneNumber/app.js
@@ -0,0 +1,22 @@
+
+const multiplying = function(num1) {
+  
+};
+
+let result = multiplying(3)(5)(2);
+
+
+
+
+
+
+
+
+
+
+
+// Testing multiplying...
+
+console.assert(multiplying(3)(5)(2) === 30, `multiplying first attempt!`);
+console.assert(multiplying(6)(10)(0) === 0, `multiplying second attempt!`);
+console.assert(multiplying(1)(12)(5) === 60, `multiplying third attempt!`);
