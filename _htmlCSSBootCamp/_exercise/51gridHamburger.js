150a0b69d6be779892acce5a6b03bf770093b900
_temp <78365254+solf5978@users.noreply.github.com>
2024-01-23 23:14:24 +0800

    drawer animation worked

--- _htmlCSSBootCamp/_exercise/51gridHamburger.js
+++ _htmlCSSBootCamp/_exercise/51gridHamburger.js
@@ -3,9 +3,12 @@ const hamburgerBTN = document.querySelector(".hamburger")
 const topBar = document.querySelector(".barTOP")
 const midBar = document.querySelector(".barMID")
 const btmBar = document.querySelector(".barBTM")
+const mobileDraw = document.querySelector(".mobileNav")
 
 hamburgerBTN.addEventListener("click", () => {
     topBar.classList.toggle("animateTOPBar")
     midBar.classList.toggle("animateMIDBar")
     btmBar.classList.toggle("animateBTMBar")
+    mobileDraw.classList.toggle("openDrawer")
+
 })
