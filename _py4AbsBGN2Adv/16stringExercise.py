1b05c18feba18a0e1db6c1e4ec766032c97ec280
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-03 21:32:34 +0800

    string exercise

---
+++ _py4AbsBGN2Adv/16stringExercise.py
@@ -0,0 +1,20 @@
+"""
+Author: Jesse Warner
+Date: August 1, 2023
+
+In this challenge, you are going to create a mad lib generator! You should 
+start by printing a welcome message to the console. Then, get user input for a 
+noun, verb, adjective, and adverb and store the values in variables. After 
+that, format a string to tell a short story using the user's inputs and store 
+it in a variable. Finally, output the story to the console.
+"""
+
+# Print a welcome message to the console
+print("Welcome To Python Console")
+# Get inputs for a noun, verb, adjective, and adverb and store them in variables
+noun = input("Give Me A Noun: ")
+verb = input("Give Me A Verb")
+adj = input("Give Me An Adj")
+# Format a string be a story with the 4 user inputs and store in in a variable
+
+# Print the formatted story string to the console
