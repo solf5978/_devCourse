d0ade06f81a3823cf9b645cbaf7b69e10a969e15
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-08 21:40:45 +0800

    sorted out the raw spliter and add row

---
+++ _nnTSMC/src/03CSVWriter.ts
@@ -0,0 +1,34 @@
+
+interface Payment {
+    id: number
+    amount: number
+    to: string
+    notes: string
+}
+
+type PaymentColumns = ('id' | 'amount' | 'to' | 'notes')[]
+
+class CSVWriter {
+    constructor(private columns: PaymentColumns) {
+        this.csv = this.columns.join(',') + '\n'
+    }
+    private csv: string
+
+    addRows(values: Payment[]): void {
+        let rows = values.map((v) => this.formatRow(v))
+
+        this.csv += rows.join('\n')
+        console.log(this.csv)
+    }
+
+    private formatRow(p: Payment): string {
+        return this.columns.map((col) => p[col]).join(',')
+    }
+}
+
+const writer = new CSVWriter(['id', 'amount', 'to', 'notes'])
+
+writer.addRows([
+    { id: 1, amount: 50, to: 'yoshi', notes: 'for--'},
+    { id: 13, amount: 510, to: 'ygdsaoshi', notes: 'fordas--'},
+])
