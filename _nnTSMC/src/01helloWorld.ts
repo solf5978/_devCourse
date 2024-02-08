60fa767d3f5d1cb1da7c367fd7c03f7c7d4504c1
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-08 16:05:51 +0800

    the use of same name of the interface is considered as an IMPL

--- _nnTSMC/src/01helloWorld.ts
+++ _nnTSMC/src/01helloWorld.ts
@@ -244,3 +244,29 @@ function logDetails(value: PerUser | PerPerson) : void {
         value.firstName = "hello"
     }
 }
+
+// reusable interfaces
+
+interface hasQuantity {
+    quantity: number
+}
+
+const some1thing: hasQuantity = {quantity:50}
+function printQuantity(item: hasQuantity) : void {
+    console.log(`the qty of the item is ${item.quantity}`)
+}
+
+const fruit = {
+    name: 'mango',
+    quantity: 50,
+}
+
+const vehicle = {
+    type: 'car',
+    quantity: 3,
+}
+
+const sigPerson = {
+    name: 'mario',
+    age: 30,
+}
