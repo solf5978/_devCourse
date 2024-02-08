485973dd67dca0e2c3895af801f072b37fa95d8f
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-08 22:30:20 +0800

    generics on interfaces

---
+++ _nnTSMC/src/06genericInterface.ts
@@ -0,0 +1,22 @@
+interface Collection<T> {
+    data: T[]
+    name: string
+}
+
+const collectionOne: Collection<string> = {
+    data: ['ehlo', 'peach', 'watermelon'],
+    name: 'hello world'
+}
+
+const collectionTwo: Collection<number> = {
+    data: [1, 2,3, 4,5, 6,],
+    name: "a collection of number"
+}
+
+function randomCollectionItem<T>(c: Collection<T>): T {
+    const i = Math.floor(Math.random() * c.data.length)
+    return c.data[i]
+}
+
+const resultOne = randomCollectionItem<string>(collectionOne)
+const resultTwo = randomCollectionItem<number>(collectionTwo)
