05dd5b83ba93dd87da633f3d97902606e37bd2f1
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-08 23:05:14 +0800

    generic constraints, but still has to declare the id inside demoU interface

--- _nnTSMC/src/07genericsClass.ts
+++ _nnTSMC/src/07genericsClass.ts
@@ -1,5 +1,9 @@
 
-class DataCollection<T> {
+interface HasId {
+    id: number
+}
+
+class DataCollection<T extends HasId> {
     constructor(private data: T[]){}
 
     loadOne(): T{
@@ -15,20 +19,26 @@ class DataCollection<T> {
         this.data.push(val)
         return this.data
     }
+
+    deleteOne(id: number) : void {
+        this.data = this.data.filter((item) => item.id !== id)
+    }
 }
 
 interface demoU {
     name: string
     score: number
+    id: number
 }
 
 const gen_user = new DataCollection<demoU>([
-    { name: 'shaun', score: 125 },
-    { name: 'shaun', score: 125 },
-    { name: 'shaun', score: 125 },
+    { name: 'shaun', score: 125, id: 1 },
+    { name: 'shaun', score: 125, id: 2 },
+    { name: 'shaun', score: 125, id: 3 },
 ])
 
-gen_user.add( { name: 'shuan', score: 100})
+gen_user.add( { name: 'shuan', score: 100, id: 3})
+gen_user.deleteOne(2)
 
 console.log('load one - ', gen_user.loadOne)
 console.log('load all - ', gen_user.loadAll)
