57dae06927a0310daa82780d3fd8da4113fb9840
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-09 15:41:19 +0800

    get form data input back to the html

--- _nnTSMC/src/ts/create.ts
+++ _nnTSMC/src/ts/create.ts
@@ -1,3 +1,5 @@
+import { PizzaProps, Pizza } from "./models/Pizzas"
+
 const titleInput = document.querySelector(
     'input[name="title"]'
 ) as HTMLInputElement
@@ -10,4 +12,22 @@ const form = document.querySelector('.create') as HTMLFormElement
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
+
+    const data = new FormData(form)
+    const newPizza: PizzaProps = {
+        title: data.get('title') as string,
+        description: data.get('description') as string,
+        toppings: data.getAll('toppings') as string[],
+        price: parseInt(data.get('price') as string),
+    }
+
+    const res = await Pizza.save(newPizza)
+
+    if (!res.ok) {
+        console.log('not able to save the pizza')
+    }
+
+    if (res.ok) {
+        window.location.href = '/'
+    }
 })
--- _nnTSMC/src/ts/services/DataResource.ts
+++ _nnTSMC/src/ts/services/DataResource.ts
@@ -20,11 +20,13 @@ export class DataResource<T> {
         return res
     }
 
-    async save (data: T) {
+    async save (data: T): Promise<Response> {
         const res = await fetch(this.endpoint, {
             method: 'POST',
             body: JSON.stringify(data),
             headers: { 'Content-Type': 'application/json' }
         })
+
+        return res
     }
 }
