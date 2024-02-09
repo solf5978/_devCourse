5cc8642c6f05a02aaa12097e611f7ac8dd37aba8
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-09 15:50:34 +0800

    starter made

---
+++ _nnTSMC/ninja-blog/src/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <main>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nesciunt non veritatis impedit quos provident recusandae, porro explicabo. Incidunt officia suscipit earum adipisci beatae impedit! Praesentium sit magni quis neque.</p>
+      </main>
+    );
+  }
+  
