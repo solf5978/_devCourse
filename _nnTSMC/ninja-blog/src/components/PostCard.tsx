bbd1a2a5a289e4de3a87062f9850f4509702b863
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-09 16:00:35 +0800

    change unpakcing to use the interface which having a certain structure we can foolow

--- _nnTSMC/ninja-blog/src/components/PostCard.tsx
+++ _nnTSMC/ninja-blog/src/components/PostCard.tsx
@@ -1,4 +1,9 @@
-export default function PostCard({title, author}: {title: string, author: string}) {
+interface PostCardProps {
+    title: string
+    author: string
+}
+
+export default function PostCard({title, author}: PostCardProps) {
     return (
         <div className="card">
             <h2>{title}</h2>
