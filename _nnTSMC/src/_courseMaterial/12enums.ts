e2eeb5ac91543baa1fc734e829c7f993393969b8
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-09 16:31:58 +0800

    enums

---
+++ _nnTSMC/src/_courseMaterial/12enums.ts
@@ -0,0 +1,14 @@
+
+enum Priority {
+    Lowest = 0,
+    Low = 1,
+    Medium = 2,
+    High = 3,
+    Urgent = 4,
+}
+
+function addTicket(details: string, priority: number) {
+    
+}
+
+addTicket('issue level: ', Priority.Urgent)
