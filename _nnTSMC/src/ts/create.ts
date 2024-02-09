b9a8833934f0102541f43d26e14a9a719da14e5e
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-09 15:31:21 +0800

    different kind of type assertion

--- _nnTSMC/src/ts/create.ts
+++ _nnTSMC/src/ts/create.ts
@@ -2,4 +2,12 @@ const titleInput = document.querySelector(
     'input[name="title"]'
 ) as HTMLInputElement
 
-titleInput.value
+const descriptionInput = document.querySelector(
+    'textarea'
+) as HTMLTextAreaElement
+
+const form = document.querySelector('.create') as HTMLFormElement
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+})
