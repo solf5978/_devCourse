4d1316392b8047e4123c9d4acee9a3e00e23be7b
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-15 21:28:49 +0800

    rust core types unsigned signed integer

---
+++ _theRustPL/rust_core_types/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let unsigned_8b: u8 = 123;      // unsigned 8bits
+    let unsigned_16b: u16 = 123;    // unsigned 16bits
+
+    println!("8bits = {}", unsigned_8b);
+    println!("16bits num = {}", unsigned_16b);
+
+    //mutable value
+    let mut mut_var: i8 = 0;
+    println!("mutable var: {}", mut_var);
+    mut_var = 15;
+    println!("mutable var: {}", mut_var);
+
+
+}
