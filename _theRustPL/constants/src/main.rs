f605c142e26a5c37eda802b6997f9795eb548cd2
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-15 22:33:14 +0800

    const and static

--- _theRustPL/constants/src/main.rs
+++ _theRustPL/constants/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     const MEANING_OF_LIFE: u8 = 42;
+    static mut HELLO_WORLD: u8 = 0x25;
     // MUST GIVEN TYPE
     println!("{}", MEANING_OF_LIFE);
+    unsafe {
+        println!("{}", HELLO_WORLD);
+    }
 }
