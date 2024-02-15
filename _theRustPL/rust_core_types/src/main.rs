da042d715601635d095dc1fe2f752d853c887486
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-15 21:54:21 +0800

    print out the size of integer types

--- _theRustPL/rust_core_types/src/main.rs
+++ _theRustPL/rust_core_types/src/main.rs
@@ -1,19 +1,27 @@
 use std::mem;
 
 fn main() {
-    let unsigned_8b: u8 = 123;      // unsigned 8bits
-    let unsigned_16b: u16 = 123;    // unsigned 16bits
+    let var_u8: u8 = 0;
+    let var_u16: u16 = 0;
+    let var_u32: u32 = 0;
+    let var_u64: u64 = 0;
+    let var_i8: i8= 0;
+    let var_i16: i16 = 0;
+    let var_i32: i32 = 0;
+    let var_i64: i64 = 0;
 
-    println!("8bits = {}", unsigned_8b);
-    println!("16bits num = {}", unsigned_16b);
+    println!(" u8's value = {} , and its size is {}", var_u8, mem::size_of_val(&var_u8));
+    println!(" u16's value = {} , and its size is {}", var_u16, mem::size_of_val(&var_u16));
+    println!(" u32's value = {} , and its size is {}", var_u32, mem::size_of_val(&var_u32));
+    println!(" u64's value = {} , and its size is {}", var_u64, mem::size_of_val(&var_u64));
+    println!(" i8's value = {} , and its size is {}", var_i8, mem::size_of_val(&var_i8));
+    println!(" i16's value = {} , and its size is {}", var_i16, mem::size_of_val(&var_i16));
+    println!(" i32's value = {} , and its size is {}", var_i32, mem::size_of_val(&var_i32));
+    println!(" i64's value = {} , and its size is {}", var_i64, mem::size_of_val(&var_i64));
+    
 
-    //mutable value
-    let mut mut_var: i8 = 0;
-    println!("mutable var: {}", mut_var);
-    mut_var = 15;
-    println!("mutable var: {}", mut_var);
 
-    println!("mut_var {}, size = {} bytes", mut_var, mem::size_of_val(&mut_var));
+
 
 
 }
