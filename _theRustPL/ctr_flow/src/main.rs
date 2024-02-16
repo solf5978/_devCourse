48c2a035ff41de58568472c9e16ee90e4287ec2b
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-16 08:52:09 +0800

    if else is an expression

--- _theRustPL/ctr_flow/src/main.rs
+++ _theRustPL/ctr_flow/src/main.rs
@@ -8,7 +8,13 @@ fn if_statement()
         println!("It's hot outside");
     } else if temp < 10 {
         println!("It's cold ");
+    } else {
+        println!("It's a mild weather");
     }
+
+    // if else is an expression
+    let day:str = if temp > 20 {"sunny"} else {"cloudy"};
+    println!("today is {}", day);
 }
 
 fn main() {
