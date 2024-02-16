486b83baad8d495ded646e16ae619ddcfb03e68c
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-16 10:58:41 +0800

    nested struct

--- _theRustPL/data_structure/src/main.rs
+++ _theRustPL/data_structure/src/main.rs
@@ -4,11 +4,17 @@ struct Point {
     y : f64,
 }
 
+struct Line {
+    start: Point,
+    end: Point,
+}
+
 fn structures() {
     let p = Point { x: 3.0, y: 4.0 };
     println!("point p is at ({} , {})", p.x, p.y);
 
     let p2 = Point { x: 5.0, y: 10.0 };
+    let myline = Line { start: p, end: p2 };
 }
 
 fn main() {
