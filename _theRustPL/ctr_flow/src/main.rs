cc4eb026e261d0c2469eb7658ba1e704e9531e71
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-16 08:55:52 +0800

    nested if

--- _theRustPL/ctr_flow/src/main.rs
+++ _theRustPL/ctr_flow/src/main.rs
@@ -15,6 +15,13 @@ fn if_statement()
     // if else is an expression
     let day:str = if temp > 20 {"sunny"} else {"cloudy"};
     println!("today is {}", day);
+
+    println!("is it {}"
+             ,if temp > 20 {"hot"} else if temp < 10 {"cold"} else {"ok"});
+
+    println!("it is {}", if temp > 20 {
+        if temp > 30 {"super hot"} else {"just hot"}
+        else if temp < 10 {{"cold"} else {"ok"}});
 }
 
 fn main() {
