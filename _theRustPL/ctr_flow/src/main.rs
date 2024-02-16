66784d8d7b6453c89bcf19665c9142774d4eaf37
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-16 08:28:10 +0800

    basic control flow with if else

---
+++ _theRustPL/ctr_flow/src/main.rs
@@ -0,0 +1,16 @@
+
+
+fn if_statement()
+{
+    let temp = 25;
+    if temp > 30
+    {
+        println!("It's hot outside");
+    } else if temp < 10 {
+        println!("It's cold ");
+    }
+}
+
+fn main() {
+    if_statement();
+}
