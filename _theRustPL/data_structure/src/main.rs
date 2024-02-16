0a9db5d503574616c321a9a216d7835a6cc2ce70
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-16 14:09:14 +0800

    array in mem, and multi arrays

--- _theRustPL/data_structure/src/main.rs
+++ _theRustPL/data_structure/src/main.rs
@@ -1,3 +1,4 @@
+use std::mem;
 
 struct Point {
     x : f64,
@@ -77,6 +78,19 @@ fn create_array() {
     if a != [1, 2, 3, 4, 5] {
         println!("mismatch");
     }
+
+    let b = [1u8; 10]; //having 10 elements with value of 1
+    for i in 0..b.len() {
+        println!("{}", b[i]);
+    }
+
+    println!("b took up {} bytes", mem::size_of_val(&b));
+
+    let mtx:[[f32;3]; 2] = [
+        [1.0, 0.0, 0.0],
+        [0.0, 2.0, 0.0]
+    ];
+    println!("{:?}", mtx);
 
 }
 
