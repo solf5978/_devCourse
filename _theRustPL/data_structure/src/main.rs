2a948ee3f0db7d7c349c53081fb81ac3c3578221
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-16 11:25:42 +0800

    enumerations

--- _theRustPL/data_structure/src/main.rs
+++ _theRustPL/data_structure/src/main.rs
@@ -14,7 +14,25 @@ fn structures() {
     println!("point p is at ({} , {})", p.x, p.y);
 
     let p2 = Point { x: 5.0, y: 10.0 };
-    let myline = Line { start: p, end: p2 };
+    let my_line = Line { start: p, end: p2 };
+}
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+    RGBColor(u8, u8, u8)
+}
+
+fn enums() {
+    let c:Color = Color::Red;
+
+    match c {
+        Color::Red => println!("RED COLOR"),
+        Color::Green => println!("GREEN COLOR"),
+        Color::Blue => println!("BLUE COLOR"),
+        _ => println!("exhaustive"),
+    }
 }
 
 fn main() {
