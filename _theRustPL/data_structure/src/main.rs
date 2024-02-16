b3d59351600bbcdb6f24c1ab429707d495ced6d3
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-16 12:59:42 +0800

    tuple inside a enum and struct inside a enum

--- _theRustPL/data_structure/src/main.rs
+++ _theRustPL/data_structure/src/main.rs
@@ -21,16 +21,23 @@ enum Color {
     Red,
     Green,
     Blue,
-    RGBColor(u8, u8, u8)
+    RGBColor(u8, u8, u8),
+    Cmyk {
+        cyan: u8, magenta: u8, yellow: u8, black: u8
+    },
+
 }
 
 fn enums() {
     let c:Color = Color::Red;
+    let cmyk:Color = Color::Cmyk {cyan: 9, magenta: 128, yellow: 0, black: 0};
 
     match c {
         Color::Red => println!("RED COLOR"),
         Color::Green => println!("GREEN COLOR"),
         Color::Blue => println!("BLUE COLOR"),
+        Color::RGBColor(r, g ,b ) => println!("RGB({}, {} {})", r, g, b),
+        Color::Cmyk {cyan:_, magenta:_, yellow:_, black:_} => println!("cmyk"),
         _ => println!("exhaustive"),
     }
 }
