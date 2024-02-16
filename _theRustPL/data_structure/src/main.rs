e71d699df9f9cabdbe13181a3a5396d3eac30ea9
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-16 14:20:41 +0800

    let the array be mutable

--- _theRustPL/data_structure/src/main.rs
+++ _theRustPL/data_structure/src/main.rs
@@ -101,13 +101,16 @@
 //     }
 // }
 
-fn use_slice(slice: &[i32]) {
+fn use_slice(slice: &mut [i32]) {
     println!("first elem = {}, len = {}", slice[0], slice.len());
+    slice[3] = 13
 }
 
 fn slice() {
     let mut data = [1, 2, 3, 4, 5];
-    use_slice(&data[1..4]);
+    // use_slice(&mut data[1..4]);
+    use_slice(&mut data);
+    println!("{:?}", data);
 }
 
 fn main() {
