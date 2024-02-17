6b26a81ce86d10a92d9dbd82a5240f98a312e16d
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-17 10:14:54 +0800

    becareful with the ownership cahnges during calling the closures

--- _theRustPL/functions/src/main.rs
+++ _theRustPL/functions/src/main.rs
@@ -39,8 +39,26 @@ fn functions() {
     println!("p -> {} ", p);
 }
 
+fn say_hello() { println!("hello"); }
+fn closures() {
+    say_hello()
+    let haha = say_hello;
+    sh();
+
+    let plus_one = |x: i32| -> i32 { x + 1 };
+    let a = 6;
+    println!("{} + 1 = {}", a, plus_one(a));
+
+    let plus_two = |x| {
+        let mut z = x;
+        z += 2;
+        z
+    }; println!("{}  + 2 = {}", 3, plus_two(3));
+}
+
 fn main() {
     println!("Hello, world!");
 
     functions();
+    closures();
 }
