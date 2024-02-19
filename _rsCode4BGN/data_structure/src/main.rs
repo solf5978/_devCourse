a45e1bf89394682607aa80d53dd2f97fd4a879ce
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-19 22:38:25 +0800

    get output from enum

--- _rsCode4BGN/data_structure/src/main.rs
+++ _rsCode4BGN/data_structure/src/main.rs
@@ -2,6 +2,22 @@
 enum Direction {
     Left,
     Right,
+    Up,
+}
+
+enum Color {
+    Red,
+    Yellow,
+    Blue,
+}
+
+fn print_color(my_color: Color) -> String {
+    match my_color {
+        Color::Red => "Red".to_string(),
+        Color::Yellow => "小黃雞".to_string(),
+        Color::Blue => "Blue".to_string(),
+
+    }
 }
 
 fn main() {
@@ -9,5 +25,9 @@ fn main() {
     match next_move {
         Direction::Left => println!("go left"),
         Direction::Right => println!("go right"),
+        Direction::Up =>println!("go up"),
+
     }
+    let output = print_color(Color::Yellow);
+    println!("{}", output);
 }
