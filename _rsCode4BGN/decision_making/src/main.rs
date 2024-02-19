15d158918a718f16fecc1dcb6d84c59f7f5299fd
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-19 21:52:57 +0800

    if else if else and match

--- _rsCode4BGN/decision_making/src/main.rs
+++ _rsCode4BGN/decision_making/src/main.rs
@@ -6,4 +6,21 @@ fn main() {
     } else {
         println!("goodbye");
     }
+
+    let n = 8;
+    if n > 5 {
+        println!(">5");
+    } else if n < 5 {
+        println!("<5");
+    } else {
+        println!("=5");
+    }
+
+    let some_bool = true;
+    match some_bool {
+        true => println!("its true"),
+        false => println!("its false"),
+    }
+
+    
 }
