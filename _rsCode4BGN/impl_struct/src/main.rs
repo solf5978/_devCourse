12ee66ed4d64c59bb651feb3468e6504aa508a96
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-20 12:05:17 +0800

    combine things altogether

---
+++ _rsCode4BGN/impl_struct/src/main.rs
@@ -0,0 +1,54 @@
+
+enum Color {
+    Brown,
+    Red,
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Brown => println!("BROWN"),
+            Color::Red => println!("RED"),
+        }
+    }
+}
+
+struct Dimensions {
+    width: f64,
+    height: f64, 
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("Width: {:?}", self.width);
+        println!("Height: {:?}", self.height);
+        println!("Depth: {:?}", self.depth);
+    }
+}
+
+struct ShippingBox {
+    color: Color,
+    weight: f64,
+    dimensions: Dimensions,
+}
+
+impl ShippingBox {
+    fn new(weight: f64, color: Color, dimentsions: Dimensions) -> Self {
+        Self {
+            weight,
+            color,
+            dimentsions,
+        }
+    }
+
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+    }
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
