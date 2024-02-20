d3d7566d6b6d1fae70ec1546241fa30df89b37ce
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-20 22:51:06 +0800

    string collection using method of type(struct)

--- _rsCode4BGN/data_collections/src/main.rs
+++ _rsCode4BGN/data_collections/src/main.rs
@@ -1,3 +1,18 @@
+
+struct Person {
+    name: String,
+    fav_color: String,
+    age: u8,
+}
+
+impl Person {
+    fn props(&self) {
+        println!("{}", (self.name));
+        println!("{}", (self.fav_color));
+        println!("{}", (self.age));
+    }
+}
+
 fn main() {
     let my_num = vec![10, 20, 30, 40];
 
@@ -9,5 +24,27 @@ fn main() {
     }
     println!("number of elements = {:?}", my_num.len());
 
-    
+    let people = vec![
+        Person {
+            name: String::from("george"),
+            fav_color: String::from("Green"),
+            age: 8,
+        },
+        Person {
+            name: String::from("Anna"),
+            fav_color: String::from("Purple"),
+            age: 9,
+        },
+        Person {
+            name: String::from("Katie"),
+            fav_color: String::from("Blue"),
+            age: 14,
+        },
+    ];
+
+    for person in &people {
+        if person.age <= 10 {
+            person.props();
+        }
+    }
 }
