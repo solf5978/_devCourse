d605f3957413e45de4f2f25f81d9ff9f553c014a
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-20 22:57:35 +0800

    derive concept

---
+++ _rsCode4BGN/advConcept/src/main.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+enum Position {
+    Manager,
+    Supervisor,
+    Worker,
+}
+
+struct Employee {
+    pos: Position,
+    working_hours: u32,
+}
+
+fn main() {
+    let me = Employee {
+        pos: Position::Worker,
+        working_hours: 40,
+    };
+
+    match me.pos {
+        Position::Manager => println!("A Manager"),
+        Position::Supervisor => println!("A Supervisor"),
+        Position::Worker => println!("A Worker"),
+    }
+}
