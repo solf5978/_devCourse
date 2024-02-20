bab3e5faa2a41c071e22551e38825d8a3a07bddc
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-20 23:00:32 +0800

    using derive on struct, but shall be the debug always exists?

--- _rsCode4BGN/advConcept/src/main.rs
+++ _rsCode4BGN/advConcept/src/main.rs
@@ -5,6 +5,7 @@ enum Position {
     Worker,
 }
 
+#[derive(Debug)]
 struct Employee {
     pos: Position,
     working_hours: u32,
@@ -16,9 +17,15 @@ fn main() {
         working_hours: 40,
     };
 
-    match me.pos {
-        Position::Manager => println!("A Manager"),
-        Position::Supervisor => println!("A Supervisor"),
-        Position::Worker => println!("A Worker"),
-    }
+    // match me.pos {
+    //     Position::Manager => println!("A Manager"),
+    //     Position::Supervisor => println!("A Supervisor"),
+    //     Position::Worker => println!("A Worker"),
+    // }
+
+    //Using #[derive(Debug)] on Enum
+    println!("{:?}", me.pos);
+
+    //Using #[derive(Debug)] on Struct
+    println!("{:?}", me);
 }
