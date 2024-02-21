e382b34bc5ede5545111adcf55cccd8e5c591737
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-21 23:38:50 +0800

    options with struct

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -4,7 +4,24 @@ struct Survey {
     q3: Option<String>,
 }
 
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
+
 fn main() {
+    let mary = Student {
+        name: "Mary".to_owned(),
+        locker: Some(3),
+    };
+
+    println!("student: {:?}", mary.name);
+    match mary.locker {
+        Some(num) => println!("locker number -> {:?}", num),
+        None => println!("no locker assigned"),
+    }
+
+
     println!("Hello, world!");
     let rep = Survey {
         q1: Some(12),
