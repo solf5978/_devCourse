93d53e07a1e3932f79a5db81840e1eec4831523e
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-21 23:27:35 +0800

    options demo case

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -1,3 +1,19 @@
+struct Survey {
+    q1: Option<i32>,
+    q2: Option<bool>,
+    q3: Option<String>,
+}
+
 fn main() {
     println!("Hello, world!");
+    let rep = Survey {
+        q1: Some(12),
+        q2: Some(true),
+        q3: Some("A".to_owned()),
+    };
+
+    match rep.q1 {
+        Some(ans) => println!("q1: {:?}", ans),
+        None => println!("q1: No rep"),
+    }
 }
