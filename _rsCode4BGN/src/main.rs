5839a06a8f82a486f82fb44cefb3f06a9297dfea
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-22 22:33:22 +0800

    create an hashmap

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -1,3 +1,9 @@
+use std::collections::HashMap;
+
+struct Contents {
+    content: String,
+}
+
 #[derive(Debug)]
 enum MenuChoice {
     MainMenu,
@@ -87,6 +93,26 @@ fn pick_choice(input: &str) -> Result<(), String> {
 }
 
 fn main() {
+    let mut lockers = HashMap::new();
+    lockers.insert(
+        1,
+        Contents {
+            content: "stuff".to_owned(),
+        },
+    );
+    lockers.insert(
+        2,
+        Contents {
+            content: "shirt".to_owned(),
+        },
+    );
+    lockers.insert(
+        3,
+        Contents {
+            content: "gym shorts".to_owned(),
+        },
+    );
+
     let mgr = Employee {
         status: Status::Active,
         position: Position::Manager,
