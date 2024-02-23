f08b80cb1840c2ba878c09d5401672ac0c89c6c1
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-23 22:17:21 +0800

    using iterator and chaining functions

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -30,6 +30,12 @@ fn part_3() -> Access {
 }
 
 fn main() {
+    let data = vec![1, 2, 3, 4, 5]
+        .iter()
+        .map(|num| num * 3)
+        .filter(|num| num > &10)
+        .collect();
+
     let a: Option<i32> = Some(1);
     let a_is_some = a.is_some();
     let a_is_none = a.is_none();
