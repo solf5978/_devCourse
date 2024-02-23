8c7b0dc7911848441d1227c654ca59f08a57996d
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-23 10:12:24 +0800

    level 1 bill, add(insert)/view(get)

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 struct Bill {
     name: String,
     amount: f64,
@@ -21,4 +22,64 @@ impl Bills {
     }
 }
 
-fn main() {}
+fn get_input() -> String {
+    let mut buf = String::new();
+    while io::stdin().read_line(&mut buf).is_err() {
+        println!("Please Re-Enter Your Data");
+    }
+    buf.trim().to_owned()
+}
+
+fn get_bill_amount() -> f64 {
+    println!("Amount: ");
+    loop {
+        let input: String = get_input();
+        let parsed_input: Result<f64, _> = input.parse();
+        match parsed_input {
+            Ok(amount) => return amount,
+            Err(_) => println!("Please Re-Enter A Number: "),
+        }
+    }
+}
+
+fn add_bill_menu(bills: &mut Bills) {
+    println!("Bill Name: ");
+    let name = get_input();
+    let amount = get_bill_amount();
+    let bill = Bill { name, amount };
+    bills.add(bill);
+    println!("Bill Added");
+}
+
+fn view_bills_menu(bills: &Bills) {
+    for bill in bills.get_all() {
+        println!("{:?}", bill);
+    }
+}
+
+fn main_menu() {
+    fn show() {
+        println!("");
+        println!("-- Manage Bills --");
+        println!("1. Add New Bill");
+        println!("2. View Bills");
+        println!("");
+        println!("Selection:");
+    }
+
+    let mut bills = Bills::new();
+
+    loop {
+        show();
+        let input = get_input();
+        match input.as_str() {
+            "1" => add_bill_menu(&mut bills),
+            "2" => view_bills_menu(&bills),
+            _ => break,
+        }
+    }
+}
+
+fn main() {
+    main_menu();
+}
