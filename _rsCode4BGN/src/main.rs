0c0153074e2be49fcbb9923e92c4bfca087e6412
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-23 22:06:15 +0800

    basic foundation

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -1,3 +1,34 @@
+#[derive(Debug, Eq, PartialEq)]
+enum Access {
+    Admin,
+    User,
+    Guest,
+}
+
+fn maybe_access(name: &str) -> Option<Access> {
+    match name {
+        "admin" => Some(Access::Admin),
+        "gary" => Some(Access::User),
+        _ => None,
+    }
+}
+
+fn root() -> Option<Access> {
+    Some(Access::Admin)
+}
+
+fn part_1() -> bool {
+    maybe_access("admin").is_some()
+}
+
+fn part_2() -> Option<Access> {
+    maybe_access("root")
+}
+
+fn part_3() -> Option<Access> {
+    maybe_access("alice")
+}
+
 fn main() {
     let a: Option<i32> = Some(1);
     let a_is_some = a.is_some();
@@ -6,4 +37,25 @@ fn main() {
     let a_filtered = a.filter(|num| num == &1);
     let a_or_else = a.or_else(|| Some(5));
     let unwrapper = a.unwrap_or_else(|| 0);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+    #[test]
+    fn chk_part_1() {
+        assert_eq!(part_1(), true, "Admins have an access level");
+    }
+    #[test]
+    fn chk_part_2() {
+        assert_eq!(
+            part_2(),
+            Some(Access::Admin),
+            "Root have admin access level"
+        );
+    }
+    #[test]
+    fn chk_part_3() {
+        assert_eq!(part_3(), None, "Guest User");
+    }
 }
