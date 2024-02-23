8713820d1af9d8a45314a39900d944f1147f720b
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-23 21:00:31 +0800

    foundation of map combinator

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -1,6 +1,22 @@
 fn add_fn(a: i32, b: i32) -> i32 {
     a + b
 }
+
+#[derive(Debug)]
+struct User {
+    user_id: i32,
+    name: String,
+}
+
+fn find_user(name: &str) -> Option<i32> {
+    let name = name.to_lowercase();
+    match name.as_str() {
+        "sam" => Some(1),
+        "matt" => Some(2),
+        "katie" => Some(9),
+        _ => None,
+    }
+}
 fn main() {
     let add = |a: i32, b: i32| -> i32 { a + b };
     let add = |a, b| a + b;
