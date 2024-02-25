1b6f8269268d7ddff05f68b9ec3d39b4c9c099f3
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 00:52:05 +0800

    using slice

---
+++ _rsCompBGNGuide/courseMaterial/017slice.rs
@@ -0,0 +1,19 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+fn main() {
+    let numbers = [1, 2, 3, 4, 5];
+    let slice = &numbers[1..4];
+    println!("{:?}", slice);
+
+    let mut colors = ["red", "green", "blue", "pink"];
+    println!("{:?}", colors);
+    update_colors(&mut colors[2..4]);
+    println!("{:?}", colors);
+}
+
+fn update_colors(colors_slice: &mut [&str]) {
+    colors_slice[0] = "yellow";
+    colors_slice[1] = "orange";
+    // colors_slice[2] = "brown";
+}
