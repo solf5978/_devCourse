79c4eb1399ed15c4bed0e6f6f0e9304f33936391
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-25 19:09:29 +0800

    parse records

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -34,6 +34,27 @@ enum ParseError {
     MissingField(String),
 }
 
+fn parse_records(records: String, verbose: bool) -> Records {
+    let mut recs = Records::new();
+    for (num, record) in records.split('\n').enumerate() {
+        if record != "" {
+            match parse_record(record) {
+                Ok(rec) => recs.add(rec),
+                Err(e) => {
+                    if verbose {
+                        print!(
+                            "error on line number {} : {}\n > \"{}\"\n",
+                            num + 1,
+                            e,
+                            record
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
+
 fn load_records(file_name: PathBuf, verbose: bool) -> std::io::Result<Records> {
     let mut file = File::open(file_name)?;
     let mut buffer = String::new();
