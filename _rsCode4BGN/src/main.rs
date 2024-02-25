dff98f64fd12bb5ab5e83be3091a7a6b8dc08d38
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-25 18:24:11 +0800

    custom error type starter

--- _rsCode4BGN/src/main.rs
+++ _rsCode4BGN/src/main.rs
@@ -1,8 +1,50 @@
+use std::{error, result};
+
+use thiserror::Error;
 #[derive(Debug, Eq, PartialEq)]
 enum Access {
     Admin,
     User,
     Guest,
+}
+
+enum ProgramError {}
+#[derive(Debug, Error)]
+enum MenuError {
+    #[error("menu item not found")]
+    NotFound,
+}
+
+#[derive(Debug, Error)]
+enum MathError {
+    #[error("divide by zero error")]
+    DivideByZero,
+}
+
+fn pick_menu(choice: &str) -> Result<i32, MenuError> {
+    match choice {
+        "1" => Ok(1),
+        "2" => Ok(2),
+        "3" => Ok(3),
+        _ => Err(MenuError::NotFound),
+    }
+}
+
+fn divide(a: i32, b: i32) -> Result<i32, MathError> {
+    if b != 0 {
+        Ok(a / b)
+    } else {
+        Err(MathError::DivideByZero)
+    }
+}
+
+fn run(step: i32) -> Result<(), ProgramError> {
+    if step == 1 {
+        pick_menu("4")?;
+    } else if step == 2 {
+        divide(1, 0)?;
+    }
+    Ok(())
 }
 
 fn maybe_access(name: &str) -> Option<Access> {
@@ -57,6 +99,8 @@ fn print_perimeter(shape: impl Perimeter) {
 }
 
 fn main() {
+    println!("{:?}", run(1));
+    println!("{:?}", run(2));
     let square = Square { side: 5 };
     let triangle = Triangle {
         side_a: 2,
