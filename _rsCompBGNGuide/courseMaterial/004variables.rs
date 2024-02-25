704d544ada20c5bbcf6389c4aec1f71f92b6dbfd
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 00:39:21 +0800

    varaibles

---
+++ _rsCompBGNGuide/courseMaterial/004variables.rs
@@ -0,0 +1,21 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+fn main() {
+    let name = "Alex";
+    let mut age = 32;
+
+    let amount: i64 = 9387592743847;
+    println!("{}", age);
+
+    age = 43;
+
+    println!("{}", age);
+
+    let color = "blue";
+    let color = 86;
+
+    println!("{}", color);
+
+    let (a, b, c) = (43, 85, "red");
+}
