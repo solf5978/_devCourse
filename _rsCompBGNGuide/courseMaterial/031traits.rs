97a8e3f2f1cae5ed1c3bd5710f9205d62d102671
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 10:48:07 +0800

    traits are like abstract class or interface, that give a little decor on the struct

---
+++ _rsCompBGNGuide/courseMaterial/031traits.rs
@@ -0,0 +1,54 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+struct RustDev {
+    awesome: bool
+}
+
+struct JavaDev {
+    awesome: bool
+}
+
+trait Developer {
+    fn new(awesome: bool) -> Self;
+    fn language(&self) -> &str;
+    fn say_hello(&self) { println!("Hello world!") }
+}
+
+impl Developer for RustDev {
+    fn new(awesome: bool) -> Self {
+        RustDev { awesome: awesome }
+    }
+
+    fn language(&self) -> &str {
+        "Rust"
+    }
+
+    fn say_hello(&self) {
+        println!("println!(\"Hello world!\");");
+    }
+}
+
+impl Developer for JavaDev {
+    fn new(awesome: bool) -> Self {
+        JavaDev { awesome: awesome }
+    }
+
+    fn language(&self) -> &str {
+        "Java 1.8"
+    }
+
+    fn say_hello(&self) {
+        println!("System.out.println(\"Hello world!\");");
+    }
+}
+
+fn main() {
+    // let r = RustDev { awesome: true};
+    let r = RustDev::new(true);
+    let j = JavaDev::new(false);
+    println!("{}", r.language());
+    r.say_hello();
+    println!("{}", j.language());
+    j.say_hello();
+}
