a3462cad8cb65c9ed136a3aa8c3c03ca916b95c5
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 10:58:11 +0800

    cannot return trait, alternatively you can use Box with dyn keyword, having a pointer to the address to the trait of the instance wnats

---
+++ _rsCompBGNGuide/courseMaterial/033returnTrait.rs
@@ -0,0 +1,34 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+struct Dog {}
+struct Cat {}
+
+trait Animal {
+    fn make_noise(&self) -> &'static str;
+}
+
+impl Animal for Dog {
+    fn make_noise(&self) -> &'static str {
+        "woof"
+    }
+}
+
+impl Animal for Cat {
+    fn make_noise(&self) -> &'static str {
+        "meow"
+    }
+}
+
+fn get_animal(rand_number: f64) -> Box<dyn Animal> {
+    if rand_number < 1.0 {
+        Box::new(Dog {} )
+    } else {
+        Box::new( Cat {} )
+    }
+}
+
+fn main() {
+    println!("The animal says {}", get_animal(0.5).make_noise());
+    println!("The animal says {}", get_animal(2.0).make_noise());
+}
