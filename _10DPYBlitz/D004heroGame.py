fd659fc122bdd2ca626375fbb9be289ec665c939
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 22:43:14 +0800

    start the beginning of text-based hero game

---
+++ _10DPYBlitz/D004heroGame.py
@@ -0,0 +1,9 @@
+
+hero_name = input("Give your hero a name, courageous adventurer? >")
+print(f"What would you like to do, {hero_name}?")
+print(f"1. Fight a monster to gain XP")
+print(f"2. Go to the shop")
+print(f"3. Polish your weapon")
+print(f"4. Rest in hotel")
+
+action = input(" > ")
