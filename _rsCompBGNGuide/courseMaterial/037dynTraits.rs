067b84ba036e293cf1ab84362bda2eeb8b4fac59
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 11:15:23 +0800

    compile and run time dispatching on traits

---
+++ _rsCompBGNGuide/courseMaterial/037dynTraits.rs
@@ -0,0 +1,35 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+trait Duplicateable {
+    fn dupl(&self) -> String;
+}
+
+impl Duplicateable for String {
+    fn dupl(&self) -> String {
+        format!("{0}{0}", *self)
+    }
+}
+
+impl Duplicateable for i32{
+    fn dupl(&self) -> String {
+        format!("{}", *self * 2)
+    }
+}
+
+// fn duplicate<T: Duplicateable> (x: T) {
+//     println!("{}", x.dupl());
+// }
+
+fn duplicate(x: &dyn Duplicateable) {
+    println!("{}", x.dupl());
+}
+
+fn main() {
+    let a = 42;
+    let b = "Hi John ".to_string();
+    // duplicate(a);
+    // duplicate(b);
+    duplicate(&a);
+    duplicate(&b);
+}
