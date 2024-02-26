f5f3202d66310920c84d4d6ed908031a4f9700d5
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 15:19:57 +0800

    adding round on it

--- _100DoCCompPYProBC/D002tipCalculator.py
+++ _100DoCCompPYProBC/D002tipCalculator.py
@@ -3,5 +3,5 @@ total = input("What was the total bill? $\n")
 total = float(total)
 tip_perc = int(input("What percentage tip would you like to give? 10, 12, or 15?\n"))
 hc_o_ppl = int(input("How many people to split the bill?\n"))
-total_pay_person = (total + (total / tip_perc)) / hc_o_ppl
+total_pay_person = round((total + (total / tip_perc)) / hc_o_ppl)
 print(f"Each person should pay: {total_pay_person}")
