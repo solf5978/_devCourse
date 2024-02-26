56e202dbaf73e4e2a4c05dd94ad15113fe39db94
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 11:21:54 +0800

    borrowing value from variables

---
+++ _rsCompBGNGuide/courseMaterial/039borrowingValue.rs
@@ -0,0 +1,18 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+fn main() {
+    let mut a = 6;
+    {
+        let b = &mut a;
+        println!("{}", *b);
+        *b += 2;
+    }
+    println!("{}", a);
+
+    let mut v = vec![1, 2, 3, 4, 5];
+    for i in &v {
+        println!("{}", i);
+        v.push(6);
+    }
+}
