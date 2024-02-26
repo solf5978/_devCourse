3978d42526c364db19cdff47606ac6347efbf3df
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-26 10:40:39 +0800

    marcos

---
+++ _rsCompBGNGuide/courseMaterial/030writingMarcos.rs
@@ -0,0 +1,37 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+macro_rules! my_macro {
+    () => (println!("First macro"))
+}
+
+// macro_rules! name {
+//     ($name: expr) => { println!("Hey {}", $name)}
+// }
+
+macro_rules! name {
+    ($($name: expr),*) => ( $(println!("Hey {}", $name);)* )
+}
+
+macro_rules! xy {
+    (x => $e: expr) => (println!("X is {}", $e));
+    (y => $e: expr) => (println!("Y is {}", $e));
+}
+
+macro_rules! build_fn {
+    ($fn_name: ident) => {
+        fn $fn_name() {
+            println!("{:?} was called", stringify!($fn_name));
+        }
+    }
+}
+
+fn main() {
+    my_macro!();
+    name!("John");
+    name!("Alex", "Mary", "Carol");
+    xy!(x => 5);
+    xy!(y => 3 * 9);
+    build_fn!(hey);
+    hey();
+}
