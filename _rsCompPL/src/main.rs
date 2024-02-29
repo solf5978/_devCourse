30999975468d3591651f7aebdc7f98d7936531bf
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-29 13:58:44 +0800

    without pass by ref, statement inside the function takes no effort

--- _rsCompPL/src/main.rs
+++ _rsCompPL/src/main.rs
@@ -5,9 +5,19 @@ fn check_val(val: &Vec<i8>) -> bool {
         return false;
     }
 }
+
+fn add_two(v: i8) {
+    v + 2;
+}
 fn main() {
     let mut vec = vec![1, 3, 5, 7];
     println!("{:?}", check_val(&vec));
     vec.push(15);
     println!("{:?}", vec);
+
+    let mut value = 2;
+    add_two(value);
+    println!("{}", value);
+    value = 5;
+    println!("{}", value);
 }
