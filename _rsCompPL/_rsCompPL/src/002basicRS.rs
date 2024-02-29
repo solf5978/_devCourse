41bb0fe6db26d03994aa94cbd61385fe3796107b
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-29 10:22:29 +0800

    got no response from rust analyzer

--- _rsCompPL/_rsCompPL/src/002basicRS.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let val1 = 5;
-    let val2 = 2;
-    let ans = val1 % val2;
-
-    println!("{}", ans)
-}
---
+++ _rsCompPL/src/002basicRS.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let val1 = 5;
+    let val2 = 2;
+    let ans = val1 % val2;
+
+    println!("{}", ans);
+
+    let mut vec = vec![2, 4, 6, 8, 10];
+    println!("{:?}", vec);
+    vec.pop();
+    vec.push(12);
+    println!("{:?}", vec);
+
+    let str1 = String::from("Hello");
+}
+
+fn concat_string() {}
