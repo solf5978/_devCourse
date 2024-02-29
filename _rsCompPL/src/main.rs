23596711f51939f8eb45c165656f7856e2d88fc3
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-29 13:38:06 +0800

    principle of borrowing and ownership

--- _rsCompPL/src/main.rs
+++ _rsCompPL/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+fn check_val(val: Vec<i8>) -> bool {
+    if val[0] == 1 {
+        return true;
+    } else {
+        return false;
+    }
+}
+fn main() {
+    let mut vec = vec![1, 3, 5, 7];
+    println!("{:?}", check_val(vec));
+    vec.push(15);
+    println!("{:?}", vec);
+}
