815fda1cf544020160d4b1b446cd96ca384efc47
_temp <78365254+solf5978@users.noreply.github.com>
2024-02-29 17:20:38 +0800

    simple way to impl the struct with new method

--- _rsCompPL/src/main.rs
+++ _rsCompPL/src/main.rs
@@ -1,23 +1,17 @@
-fn check_val(val: &Vec<i8>) -> bool {
-    if val[0] == 1 {
-        return true;
-    } else {
-        return false;
+struct Car {
+    mpg: i8,
+    color: String,
+    top_speed: i16,
+}
+
+impl Car {
+    fn new(mpg: i8, color: String, top_speed: i16) -> Self {
+        Self {
+            mpg,
+            color,
+            top_speed,
+        }
     }
 }
 
-fn add_two(v: i8) {
-    v + 2;
-}
-fn main() {
-    let mut vec = vec![1, 3, 5, 7];
-    println!("{:?}", check_val(&vec));
-    vec.push(15);
-    println!("{:?}", vec);
-
-    let mut value = 2;
-    add_two(value);
-    println!("{}", value);
-    value = 5;
-    println!("{}", value);
-}
+fn main() {}
