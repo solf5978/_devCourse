ad6b74479b4a23404ae174098cabee88cf98705d
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-01 10:15:29 +0800

    trait on both car and moto

--- _rsCompPL/src/main.rs
+++ _rsCompPL/src/main.rs
@@ -1,1 +1,63 @@
+#[derive(Debug)]
+struct Car {
+    mpg: i8,
+    color: String,
+    top_speed: i16,
+}
 
+#[derive(Debug)]
+struct MotoCycle {
+    mpg: i8,
+    color: String,
+    top_speed: i16,
+}
+
+pub trait Properties {
+    fn set_mpg(&mut self, mpg: i8);
+    fn set_color(&mut self, color: String);
+    fn set_top_speed(&mut self, top_speed: i16);
+}
+
+impl Properties for MotoCycle {
+    fn set_mpg(&mut self, mpg: i8) {
+        self.mpg = mpg;
+    }
+    fn set_color(&mut self, color: String) {
+        self.color = color;
+    }
+    fn set_top_speed(&mut self, top_speed: i16) {
+        self.top_speed = top_speed;
+    }
+}
+
+impl Properties for Car {
+    fn set_mpg(&mut self, mpg: i8) {
+        self.mpg = mpg;
+    }
+    fn set_color(&mut self, color: String) {
+        self.color = color;
+    }
+    fn set_top_speed(&mut self, top_speed: i16) {
+        self.top_speed = top_speed;
+    }
+}
+
+impl Car {
+    fn new(mpg: i8, color: String, top_speed: i16) -> Self {
+        Self {
+            mpg,
+            color,
+            top_speed,
+        }
+    }
+}
+
+fn main() {
+    let car1 = Car {
+        mpg: 0,
+        color: String::from("Red"),
+        top_speed: 0,
+    };
+
+    println!("{:?}", car1);
+}
