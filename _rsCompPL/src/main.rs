00ad8311816ac2e6dc50955510376b620fb49bfd
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-02 15:12:34 +0800

    ref count

--- _rsCompPL/src/main.rs
+++ _rsCompPL/src/main.rs
@@ -1,5 +1,13 @@
+use std::rc::Rc;
 fn main() {
     let val = 5;
     let val2 = Box::new(2);
     println!("{}", val * *val2);
+
+    let rc_value = String::from("RC Value");
+
+    {
+        let rc: Rc<String> = Rc::new(rc_value);
+        println!("{}", Rc::strong_count(&rc));
+    }
 }
