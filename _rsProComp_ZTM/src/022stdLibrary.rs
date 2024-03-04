958acd99098ee38b39cf970559e8f10d7122b819
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-04 17:04:00 +0800

    the use of std library

---
+++ _rsProComp_ZTM/src/022stdLibrary.rs
@@ -0,0 +1,7 @@
+use std::String;
+
+fn main() {
+    let one_str = "this is a string slice";
+    println!("uppercase; {:?}", one_str.to_uppercase());
+    println!("lowercase; {:?}", one_str.to_lowercase());
+}
