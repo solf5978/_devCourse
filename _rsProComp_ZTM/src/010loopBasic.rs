bcc2be689a508f4967ab24bddd002606ef66513a
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-04 09:40:18 +0800

    fixed semicolon

--- _rsProComp_ZTM/src/010loopBasic.rs
+++ _rsProComp_ZTM/src/010loopBasic.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut num = 0;
     loop {
         if num < 5 {
-            println!("{num:?}")
+            println!("{num:?}");
             num = num + 1;
         } else {
             break;
