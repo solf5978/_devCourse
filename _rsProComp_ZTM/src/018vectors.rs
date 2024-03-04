db1b1c23e2323904287ad0bd473f589fd62bd857
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-04 15:08:17 +0800

    fulfill the requirment of vector

---
+++ _rsProComp_ZTM/src/018vectors.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let num_vec = vec![10, 20, 30, 40];
+
+    for num in num_vec {
+        if num == 30 {
+            println!("thirty");
+        } else {
+            println!("{num:?}");
+        }
+
+        match num {
+            30 => println!("thirty"),
+            _ => println!("{num:?}"),
+        }
+    }
+    println!("num of elements -> {:?}", num_vec.len());
+}
