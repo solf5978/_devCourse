bfa7d5217bbafd714b6b27f536a571eeed5c8c35
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-04 13:41:41 +0800

    ownership demo

---
+++ _rsProComp_ZTM/src/016ownership.rs
@@ -0,0 +1,18 @@
+struct GroceryItem {
+    qty: i32,
+    id: i32,
+}
+
+fn display_qty(&item: GroceryItem) {
+    println!("qty -> {:?}", item.qty);
+}
+
+fn display_id(&item: GroceryItem) {
+    println!("id -> {:?}", item.id);
+}
+
+fn main() {
+    let item1 = GroceryItem { qty: 3, id: 100 };
+    display_qty(&item1);
+    display_id(&item1);
+}
