4555a21f9466025488590d07e28000bb38ad56d0
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-05 23:10:31 +0800

    change mainmenu from function to enum

--- _rsProComp_ZTM/src/main.rs
+++ _rsProComp_ZTM/src/main.rs
@@ -1,5 +1,32 @@
 use std::io;
 
+enum MainMenu {
+    AddBill,
+    ViewBill,
+}
+
+impl MainMenu {
+    fn from_str(input: &str) -> Option<MainMenu> {
+        match input {
+            "1" => Some(Self::AddBill),
+            "2" => Some(Self::ViewBill),
+            _ => None,
+        }
+    }
+
+    fn show() {
+        println!("");
+        println!("-- Manage Bills --");
+        println!("1. Add New Bill");
+        println!("2. View Bills");
+        println!("3. Delete Bills");
+        println!("4. Update Bills");
+        println!("5. Revert Operation");
+        println!("");
+        println!("Selection:");
+    }
+}
+
 fn get_input() -> Option<String> {
     let mut buf = String::new();
     while io::stdin().read_line(&mut buf).is_err() {
@@ -13,4 +40,14 @@ fn get_input() -> Option<String> {
         Some(input)
     }
 }
-fn main() {}
+fn main() {
+    loop {
+        MainMenu::show();
+        let input = get_input().expect("No Input");
+        match MainMenu::from_str(input.as_str()) {
+            Some(MainMenu::AddBill) => (),
+            Some(MainMenu::ViewBill) => (),
+            None => return,
+        }
+    }
+}
