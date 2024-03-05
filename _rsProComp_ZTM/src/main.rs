39d09681d6dfda2fee1875aeba4833878b394daf
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-06 00:14:16 +0800

    bill struct and its' collection bills

--- _rsProComp_ZTM/src/main.rs
+++ _rsProComp_ZTM/src/main.rs
@@ -1,5 +1,29 @@
 use std::io;
+#[derive(Clone, Debug)]
+pub struct Bill {
+    name: String,
+    amount: f64,
+}
 
+pub struct Bills {
+    inner: Vec<Bill>,
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { inner: vec![] }
+    }
+
+    fn add(&mut self, bill: Bill) {
+        self.inner.push(bill);
+    }
+
+    fn get_all(&self) -> Vec<&Bill> {
+        self.inner.iter().collect()
+    }
+
+    fn view() {}
+}
 enum MainMenu {
     AddBill,
     ViewBill,
