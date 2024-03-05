2f2cf386f0160dd0e719425887d88f4d22363e88
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-05 23:05:01 +0800

    get user input and read from buffer

--- _rsProComp_ZTM/src/main.rs
+++ _rsProComp_ZTM/src/main.rs
@@ -1,1 +1,16 @@
+use std::io;
 
+fn get_input() -> Option<String> {
+    let mut buf = String::new();
+    while io::stdin().read_line(&mut buf).is_err() {
+        println!("Please re-enter your data");
+    }
+
+    let input = buf.trim().to_owned();
+    if input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+fn main() {}
