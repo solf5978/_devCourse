68866044817be666de9d6f83f393f1d4fb16ddef
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-06 19:27:33 +0800

    generic structure

--- _rsProComp_ZTM/src/038genericStrcuts.rs
+++ _rsProComp_ZTM/src/038genericStrcuts.rs
@@ -1,1 +1,37 @@
+trait Body {}
 
+trait Color {}
+#[derive(Debug)]
+struct Vehicle<T, U>
+where
+    T: Body,
+    U: Color,
+{
+    body: T,
+    color: U,
+}
+
+impl<T: Body, U: Color> Vehicle<T, U> {
+    pub fn new(body: T, color: U) -> Self {
+        Self { body, color }
+    }
+}
+#[derive(Debug)]
+struct Car;
+impl Body for Car {}
+#[derive(Debug)]
+struct Truck;
+impl Body for Truck {}
+#[derive(Debug)]
+struct Red;
+impl Color for Red {}
+#[derive(Debug)]
+struct Blue;
+impl Color for Blue {}
+
+fn main() {
+    let red_truck = Vehicle::new(Truck, Red);
+    let blue_car = Vehicle::new(Car, Blue);
+    println!("{:?}", red_truck);
+    println!("{:?}", blue_car);
+}
--- _rsProComp_ZTM/src/main.rs
+++ _rsProComp_ZTM/src/main.rs
@@ -1,19 +1,1 @@
-trait Body {}
 
-trait Color {}
-
-struct Vehicle<T, U>
-where
-    T: Body,
-    U: Color,
-{
-    body: T,
-    color: U,
-}
-
-impl<T: Body, U: Color> Vehicle<T, U> {
-    pub fn new(body: T, color: U) -> Self {
-        Self { body, color }
-    }
-}
-fn main() {}
