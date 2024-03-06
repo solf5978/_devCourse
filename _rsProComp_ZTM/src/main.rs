2a0806e806ada8270476ee584c3087b2532b6378
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-06 11:07:14 +0800

    make question mark works using Option type

--- _rsProComp_ZTM/src/main.rs
+++ _rsProComp_ZTM/src/main.rs
@@ -164,18 +164,23 @@ fn get_bill_amount() -> Option<f64> {
     }
 }
 
-fn main() {
+fn run_program() -> Option<()> {
     let mut bills = Bills::new();
     loop {
         MainMenu::show();
-        let input = get_input().expect("No Input");
+        let input = get_input()?;
         match MainMenu::from_str(input.as_str()) {
             Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
             Some(MainMenu::ViewBill) => menu::view_bills(&bills),
             Some(MainMenu::RemoveBill) => menu::remove_bill(&mut bills),
             Some(MainMenu::UpdateBill) => menu::update_bill(&mut bills),
             Some(MainMenu::RevertOperation) => (),
-            None => return,
+            None => break,
         }
     }
+    None
+}
+
+fn main() {
+    run_program();
 }
