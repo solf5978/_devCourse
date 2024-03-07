fd0fd6a5b68764dbcdf56f0d85e35ea89a95c65c
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-07 09:57:10 +0800

    custom error type

--- _rsProComp_ZTM/src/main.rs
+++ _rsProComp_ZTM/src/main.rs
@@ -1,5 +1,12 @@
 use thiserror::Error;
-enum ProgramError {}
+
+#[derive(Debug, Error)]
+enum ProgramError {
+    #[error("menu error")]
+    Menu(#[from] MenuError),
+    #[error("Math error")]
+    Math(#[from] MathError),
+}
 
 #[derive(Debug, Error)]
 enum MenuError {
@@ -20,4 +27,26 @@ fn pick_menu(choice: &str) -> Result<i32, MenuError> {
         "3" => Ok(3),
         _ => Err(MenuError::NotFound),
     }
+}
+
+fn divide(a: i32, b: i32) -> Result<i32, MathError> {
+    if b != 0 {
+        Ok(a / b)
+    } else {
+        Err(MathError::DividedByZero)
+    }
+}
+
+fn run(step: i32) -> Result<(), ProgramError> {
+    if step == 1 {
+        pick_menu("4")?;
+    } else if step == 2 {
+        divide(1, 0)?;
+    }
+    Ok(())
+}
+
+fn main() {
+    println!("{:?}", run(1));
+    println!("{:?}", run(2));
 }
