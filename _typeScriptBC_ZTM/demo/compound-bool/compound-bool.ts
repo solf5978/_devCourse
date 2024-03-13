8b480f67f247de7cf3b8baa5c7f9a39e7a1896fd
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-13 11:36:16 +0800

    compound boolean expression || &&

--- _typeScriptBC_ZTM/demo/compound-bool/compound-bool.ts
+++ _typeScriptBC_ZTM/demo/compound-bool/compound-bool.ts
@@ -1,3 +1,15 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+const writing = true;
+const reading = !writing;
+
+const rating = 9;
+const favouriteMovie = false;
+
+const suggestMovie = rating > 8 || favouriteMovie;
+assert.equal(suggestMovie, true);
+
+const age = 18;
+const isTeenager = age >= 13 && age < 20;
+assert.equal(isTeenager, true);
