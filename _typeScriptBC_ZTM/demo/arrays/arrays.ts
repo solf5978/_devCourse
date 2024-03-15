ea93c4629037bb49f420c3df7953180e6462985d
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-15 10:48:20 +0800

    array operations

--- _typeScriptBC_ZTM/demo/arrays/arrays.ts
+++ _typeScriptBC_ZTM/demo/arrays/arrays.ts
@@ -10,3 +10,10 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+let arrayDemo: number[] = [];
+const number: number[] = [1, 2, 3, 4, 5, 6];
+let letters: string[] = ["a", "b", "c", "d", "e", "f"];
+
+arrayDemo.push(1);
+arrayDemo.push(2);
