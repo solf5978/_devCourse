10b7981cad2b21018645b191fcbd0eba828c7854
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-15 14:41:26 +0800

    filter then join

--- _typeScriptBC_ZTM/app/grep/grep.ts
+++ _typeScriptBC_ZTM/app/grep/grep.ts
@@ -5,3 +5,24 @@ const filename = args[0];
 const searchString = args[1];
 
 const contents = readFileSync(filename, "utf8");
+const lines = contents.split("\n");
+
+for (let i = 0; i < lines.length; i++) {
+  const line = lines[i];
+  if (line.includes(searchString)) {
+    console.log(line);
+  }
+}
+
+for (const line of lines) {
+  if (line.includes(searchString)) {
+    console.log(line);
+  }
+}
+lines.forEach((line) => {
+  if (line.includes(searchString)) {
+    console.log(line);
+  }
+});
+
+console.log(lines.filter((line) => line.includes(searchString)).join("\n"));
