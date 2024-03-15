e2100291807275c0f4907fae8cae9d5118d07995
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-15 09:57:48 +0800

    also with type keywords

--- _typeScriptBC_ZTM/demo/objects/objects.ts
+++ _typeScriptBC_ZTM/demo/objects/objects.ts
@@ -7,3 +7,21 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/objects.html
+
+type Personal = String;
+type employee = {
+  name: Personal;
+  age: Number;
+  job: String;
+};
+
+function createEmployee(name: Personal, age: Number, job: String): employee {
+  return {
+    name: name,
+    age: age,
+    job: job,
+  };
+}
+
+let myStuff = createEmployee("John", 30, "Software Engineer");
+console.log(myStuff.name);
