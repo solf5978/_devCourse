40c085eac57ecb0b65a475489354df843a49ddb7
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-15 10:13:13 +0800

    arrow function

--- _typeScriptBC_ZTM/demo/arrow-functions/arrow-functions.ts
+++ _typeScriptBC_ZTM/demo/arrow-functions/arrow-functions.ts
@@ -6,6 +6,12 @@
 // to the parent context, making them useful in event handlers and callback
 // functions. They also support implicit return statements for one-liner
 // functions, which makes the code more readable.
-// 
+//
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+function sum(lhs: number, rhs: number): number {
+  return lhs + rhs;
+}
+
+const arrowSum = (lhs: number, rhs: number): number => lhs + rhs;
