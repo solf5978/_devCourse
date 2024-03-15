61ab0f8e7a3fab2bf35c5de4fbb8cada902b0ff4
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-15 09:40:15 +0800

    switch

--- _typeScriptBC_ZTM/demo/switch/switch.ts
+++ _typeScriptBC_ZTM/demo/switch/switch.ts
@@ -3,3 +3,47 @@ import { strict as assert } from "assert";
 
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+
+let someNumber: number = 42;
+let parseNumber: number = parseInt("42");
+
+const sample = "sample";
+const someString = `${sample}`;
+
+switch (parseNumber) {
+  case 42:
+    console.log("42");
+    break;
+  case 43:
+    console.log("43");
+    break;
+  case 44:
+    console.log("44");
+    break;
+  case 45:
+    console.log("45");
+    break;
+  case 46:
+    console.log("46");
+    break;
+  case 47:
+    console.log("47");
+    break;
+  case 48:
+    console.log("48");
+    break;
+  case 49:
+    console.log("49");
+    break;
+  case 50:
+    console.log("50");
+    break;
+  case 51:
+    console.log("51");
+    break;
+  case 52:
+    console.log("52");
+    break;
+  default:
+    console.log("unknown");
+}
