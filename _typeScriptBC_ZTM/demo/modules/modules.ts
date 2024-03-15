c25c9cf97407b490f414bd43e73d5d094808faca
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-15 14:21:31 +0800

    different ways to import / export and export with default

--- _typeScriptBC_ZTM/demo/modules/modules.ts
+++ _typeScriptBC_ZTM/demo/modules/modules.ts
@@ -8,4 +8,11 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/modules.html
 import { add, pi, Int as Integer } from "./math.js";
-import { Point } from "././coord.js";
+// import { Point } from "./coord.js";
+import Point from "./coord.js";
+// You can also use import with different name when default existed
+import Coordinate from "./coord.js";
+
+const three: Integer = 3;
+const four = add(2, 2);
+const p1: Point = { x: 1, y: 2 };
