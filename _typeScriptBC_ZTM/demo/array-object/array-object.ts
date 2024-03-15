66f2d0e7faac6e47e6eae80579c24f10af5b2faa
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-15 11:00:16 +0800

    object array

--- _typeScriptBC_ZTM/demo/array-object/array-object.ts
+++ _typeScriptBC_ZTM/demo/array-object/array-object.ts
@@ -1,2 +1,20 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
+type Links = {
+  title: string;
+  url: string;
+};
+
+const microsofts = {
+  title: "Microsoft",
+  url: "https://www.microsoft.com",
+};
+
+const typescript = {
+  title: "TypeScript",
+  url: "https://www.typescriptlang.org",
+};
+
+const links = [microsofts, typescript];
+const tsUrl = links[1].url;
+const msUrl = links[0].url;
