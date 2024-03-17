af214c4abf9a7a70469100cabbe902b9081d44b0
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-17 13:29:47 +0800

    throw exceptions

--- _typeScriptBC_ZTM/demo/exceptions/exceptions.ts
+++ _typeScriptBC_ZTM/demo/exceptions/exceptions.ts
@@ -9,3 +9,22 @@
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+
+function divide(lhr: number, rhs: number): number {
+  if (rhs === 0) {
+    throw new Error("Cannot divide by 0");
+  } else if (rhs === 1) {
+    return lhr;
+  } else {
+    return lhr / rhs;
+  }
+}
+
+try {
+  const b = divide(100, 0);
+  console.log(b);
+} catch (e) {
+  console.log(`Errir -> ${e}`);
+} finally {
+  console.log("Finally");
+}
