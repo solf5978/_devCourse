6404595fd8ffc75897be2f6992cb149fe3fe02f9
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-17 12:35:19 +0800

    can use only with constructors

--- _typeScriptBC_ZTM/demo/classes/classes.ts
+++ _typeScriptBC_ZTM/demo/classes/classes.ts
@@ -21,12 +21,28 @@ const redValue = red.r;
 
 class Dimensions {
   width: number;
-  height: number;
+  //   height: number;
   length: number;
 
-  constructor(width: number, height: number, length: number) {
+  constructor(width: number, public height: number, length: number) {
     this.width = width;
     this.height = height;
     this.length = length;
   }
 }
+
+const one = new Dimensions(1, 1, 1);
+
+class Point {
+  constructor(public x: number, public y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  translate(dx: number, dy: number): Point {
+    return new Point(this.x + dx, this.y + dy);
+  }
+}
+
+const p1 = new Point(1, 1);
+p1.translate(15, 15);
