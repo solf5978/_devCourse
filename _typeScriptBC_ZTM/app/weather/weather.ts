13196fbd3616b7c4492d7d08957c69e3efca3555
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-18 10:33:36 +0800

    fetch location data

--- _typeScriptBC_ZTM/app/weather/weather.ts
+++ _typeScriptBC_ZTM/app/weather/weather.ts
@@ -1,3 +1,5 @@
+import type { LocationInfo } from "./location";
+import { fetchLocationData } from "./location";
 const GEOCODE_API_URL = "https://maps.googleapis.com/maps/api/geocode/json";
 async function main(): Promise<number> {
   if (process.argv.length < 3) {
@@ -5,6 +7,14 @@ async function main(): Promise<number> {
     return 1;
   }
   const location = process.argv[2];
+  let locInfo: LocationInfo;
+  try {
+    locInfo = await fetchLocationData(GEOCODE_API_URL, location);
+  } catch (err) {
+    console.error(err);
+    return 1;
+  }
+  console.log(locInfo);
   return await Promise.resolve(0);
 }
 
