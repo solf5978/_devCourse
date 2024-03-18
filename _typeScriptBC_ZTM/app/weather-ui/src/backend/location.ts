713cb0565cad9dc944c43d213b60c82cb9da7b76
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-18 15:41:16 +0800

    zod will help to check our structure

--- _typeScriptBC_ZTM/app/weather-ui/src/backend/location.ts
+++ _typeScriptBC_ZTM/app/weather-ui/src/backend/location.ts
@@ -1,12 +1,16 @@
-import axios from "axios";
+import { z } from "zod";
+import type { AxiosStatic } from "axios";
 
-export interface LocationInfo {
-  displayName: string;
-  lat: number;
-  lon: number;
-}
+const locationInfoSchema = z.object({
+  lat: z.string(),
+  lon: z.string(),
+  display_name: z.string(),
+});
+
+export type LocationInfo = z.infer<typeof locationInfoSchema>;
 
 export async function fetchLocationData(
+  axios: AxiosStatic,
   apiUrl: string,
   locationName: string
 ): Promise<LocationInfo> {
@@ -17,15 +21,15 @@ export async function fetchLocationData(
       q: locationName,
     },
   };
-  const response = await axios.request<LocationInfo[]>(options);
+  const response = await axios.request(options);
 
   if (response.status === 200) {
-    if (response.data.length > 0) {
-      return response.data[0];
-    } else {
-      throw new Error("Location not found");
+    try {
+      return locationInfoSchema.parse(response.data.results[0]);
+    } catch (err) {
+      throw new Error("Failed to find location data");
     }
   } else {
-    throw new Error("Unable to fetch location data");
+    throw new Error("Failed to get location data");
   }
 }
