934617b3960fe2e51061d41b19d96574217b6449
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-20 22:55:20 +0800

    type definition files

--- _typeScriptBC_ZTM/demo/dts/dts.ts
+++ _typeScriptBC_ZTM/demo/dts/dts.ts
@@ -3,3 +3,9 @@ import { strict as assert } from "assert";
 
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html
+
+import { add, setCase, quote, max } from "./mylib";
+import type { CaseKinds } from "./mylib";
+
+const message = "Hello, world!";
+const upper = setCase(message, "sentence");
