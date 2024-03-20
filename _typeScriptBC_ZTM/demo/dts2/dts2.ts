f386eb366891c9f04c7f28142d6b8f2dc55dfa3c
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-20 23:11:13 +0800

    calling from another ts file

--- _typeScriptBC_ZTM/demo/dts2/dts2.ts
+++ _typeScriptBC_ZTM/demo/dts2/dts2.ts
@@ -3,3 +3,12 @@ import { strict as assert } from "assert";
 
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html
+
+import { apiResponse } from "./mylib";
+
+const res = apiResponse();
+if (res !== undefined) {
+  if (res.status === "success") {
+    console.log(res.data.items[1].name);
+  }
+}
