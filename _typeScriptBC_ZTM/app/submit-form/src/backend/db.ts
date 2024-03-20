9e69034714ad7fe01dbcaa8c7d9257f6f1149786
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-21 00:44:51 +0800

    creating tables

--- _typeScriptBC_ZTM/app/submit-form/src/backend/db.ts
+++ _typeScriptBC_ZTM/app/submit-form/src/backend/db.ts
@@ -13,3 +13,33 @@ export interface UserRepository {
   findByEmail(email: string): Promise<User | undefined>;
   get(userId: number): Promise<User | undefined>;
 }
+
+// export class PostgresUserRepository implements UserRepository {}
+export class SqliteUserRepository implements UserRepository {
+  constructor(private readonly db: AsyncDatabase) {}
+  create(user: User): Promise<User> {}
+  findByEmail(email: string): Promise<User | undefined> {}
+  get(userId: number): Promise<User | undefined> {}
+}
+
+export async function connect(
+  connectionString: string
+): Promise<AsyncDatabase> {
+  return await AsyncDatabase.open(connectionString);
+}
+
+export async function newDb(db: AsyncDatabase): Promise<void> {
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        agreedToTerms BOOLEAN NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS sessions (
+        session_id UUID PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+    );
+    `);
+}
