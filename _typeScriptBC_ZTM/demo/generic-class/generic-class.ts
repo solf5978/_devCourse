ba7e4b7bf71554025b6656757df788356b809814
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-20 22:38:53 +0800

    generic class

--- _typeScriptBC_ZTM/demo/generic-class/generic-class.ts
+++ _typeScriptBC_ZTM/demo/generic-class/generic-class.ts
@@ -12,3 +12,29 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-classes
+
+class Stack<T> {
+  private elements: T[] = [];
+  public push(element: T): void {
+    this.elements.push(element);
+  }
+  public pop(): T | undefined {
+    return this.elements.pop();
+  }
+  public peek(): T | undefined {
+    return this.elements[this.elements.length - 1];
+  }
+  public isEmpty(): boolean {
+    return this.elements.length === 0;
+  }
+}
+
+const strings = new Stack<string>();
+const numbers = new Stack<number>();
+strings.push("Hello");
+strings.push("World");
+strings.push("!");
+
+numbers.push(1);
+numbers.push(2);
+numbers.push(3);
