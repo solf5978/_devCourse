2df7f49bfc87a35d12504a6b3ac8e39b644dc4ef
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-20 22:44:57 +0800

    *[Symbol.iterator]() Generator<TYPE>

--- _typeScriptBC_ZTM/demo/custom-iterator/custom-iterator.ts
+++ _typeScriptBC_ZTM/demo/custom-iterator/custom-iterator.ts
@@ -4,3 +4,37 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/iterators-and-generators.html
 
+function* genValues(): Generator<number> {
+  yield 1;
+  yield 2;
+  yield 3;
+  yield 4;
+  yield 5;
+}
+
+const values = genValues();
+assert.equal(values.next().value, 1);
+assert.equal(values.next().value, 2);
+assert.equal(values.next().value, 3);
+assert.equal(values.next().value, undefined);
+
+class Range implements Iterable<number> {
+  private readonly start: number;
+  private readonly end: number;
+
+  constructor(start: number, end: number) {
+    this.start = start;
+    this.end = end;
+  }
+
+  *[Symbol.iterator](): Generator<number> {
+    for (let i = this.start; i <= this.end; i++) {
+      yield i;
+    }
+  }
+}
+
+const range = new Range(1, 5);
+for (const value of range) {
+  console.log(value);
+}
