724c27a877c4d1d37d412eb8b61dae489eff0120
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-20 16:09:48 +0800

    exhasive the msg.kind categories

--- _typeScriptBC_ZTM/demo/discriminated-unions/discriminated-unions.ts
+++ _typeScriptBC_ZTM/demo/discriminated-unions/discriminated-unions.ts
@@ -22,3 +22,19 @@ const passwordTooShort: AccountCreationMessage = {
   kind: "passwordComplexityFailure",
   message: "Password must be at least 8 characters long",
 };
+
+function showMessage(msg: AccountCreationMessage) {
+  switch (msg.kind) {
+    case "ok":
+      console.log(msg.greeting);
+      break;
+    case "passwordComplexityFailure":
+      console.log(msg.message);
+      break;
+    case "usernameExists":
+      console.log(msg);
+      break;
+    default:
+      break;
+  }
+}
