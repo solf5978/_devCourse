7f8b18bee5283b42410684776f966142caf0aac2
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-21 15:36:08 +0800

    add auth function

---
+++ _typeScriptBC_ZTM/app/submit-form/src/backend/auth.ts
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+export class HashedPassword {
+  constructor(readonly hashed: string) {}
+}
+
+export async function hasedPassword(plain: string): Promise<HashedPassword> {
+  return await new Promise((resolve, reject) => {
+    bcrypt.hash(plain, saltRounds, (err, hashed) => {
+      if (err !== undefined) {
+        reject(err);
+      } else {
+        resolve(new HashedPassword(hashed));
+      }
+    });
+  });
+}
