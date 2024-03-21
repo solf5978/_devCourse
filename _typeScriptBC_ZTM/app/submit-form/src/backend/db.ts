7e26469c501f14aec5c4090029eff049bd4f892c
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-21 09:32:18 +0800

    finished db side setup

--- _typeScriptBC_ZTM/app/submit-form/src/backend/db.ts
+++ _typeScriptBC_ZTM/app/submit-form/src/backend/db.ts
@@ -35,6 +35,32 @@ export class SqliteUserRepository implements UserRepository {
   }
 }
 
+export class SqliteSession {
+  constructor(private readonly db: AsyncDatabase) {}
+
+  async create(userId: number): Promise<string> {
+    const sessionId = uuidv4();
+    await this.db.run(
+      `INSERT INTO sessions (session_id, user_id) VALUES (?,?)`,
+      [sessionId, userId],
+      [sessionId, userId]
+    );
+    return sessionId;
+  }
+
+  async get(sessionId: string): Promise<User | undefined> {
+    const userId: { user_id: number } | undefined = await this.db.get(
+      `SELECT user_id FROM sessions WHERE session_id =?`,
+      sessionId
+    );
+    if (userId === undefined) {
+      return undefined;
+    }
+    const users = new SqliteUserRepository(this.db);
+    return await users.get(userId.user_id);
+  }
+}
+
 export async function connect(
   connectionString: string
 ): Promise<AsyncDatabase> {
